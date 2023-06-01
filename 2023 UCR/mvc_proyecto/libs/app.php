861fbeee343b8f87f22b79791f945015186c8bbf
Dago04 <dagoscr@gmail.com>
2023-06-01 10:18:02 -0600

    a

---
+++ 2023 UCR/mvc_proyecto/libs/app.php
@@ -0,0 +1,12 @@
+<?php
+    class App{
+        function __construct()
+        {
+            $url = isset($_GET['url']) ? $_GET['url'] : null;
+
+            $url = rtrim($url,'/');
+
+            $url = explode
+        }
+    }
+?>
